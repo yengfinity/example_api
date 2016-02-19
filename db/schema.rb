c0461a3eda76b12da5cad7e2926bf51853f30ee5
Yeng <leeyeng2005@gmail.com>
2016-02-18 21:10:19 -0600

    reup database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160217043857) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "customers", force: :cascade do |t|
     t.string   "full_name"
     t.string   "email"
